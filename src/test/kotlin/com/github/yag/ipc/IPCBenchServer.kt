2ae83a96d22695e3c94b26fde74bb20a7c545a10
yag <marks.yag@gmail.com>
2020-01-31 00:26:06 +0800

    Optimize benchmark

--- src/test/kotlin/com/github/yag/ipc/IPCBenchServer.kt
+++ src/test/kotlin/com/github/yag/ipc/IPCBenchServer.kt
@@ -4,6 +4,7 @@ import com.github.yag.config.ConfigLoader
 import com.github.yag.config.config
 import com.github.yag.ipc.server.IPCServerConfig
 import com.github.yag.ipc.server.server
+import io.netty.buffer.ByteBufAllocator
 import io.netty.buffer.Unpooled
 import org.apache.commons.cli.DefaultParser
 import org.apache.commons.cli.HelpFormatter
@@ -20,6 +21,8 @@ object IPCBenchServer {
             it.addOption("h", "help", false, "Show this help message.")
             it.addOption(Option.builder("f").longOpt("config").hasArg().argName("config file").desc("Configuration file path in classpath or absolute").build())
             it.addOption(Option.builder("D").argName("property=value").numberOfArgs(2).valueSeparator('=').desc("Override configuration value").build())
+            it.addOption(Option.builder("s").required(true).hasArg().argName("response-body-size").desc("Size of response body").build())
+
         }
 
         val cmd = DefaultParser().parse(options, args)
@@ -40,18 +43,15 @@ object IPCBenchServer {
             }
         }.config(IPCServerConfig::class)
 
+        val responseBodySize = cmd.getOptionValue("s").toInt()
+        val buf = ByteBufAllocator.DEFAULT.directBuffer(responseBodySize, responseBodySize).also {
+            it.writerIndex(responseBodySize)
+        }
+
         server(config) {
             request {
                 map("req") {
-                    Packet(
-                        ResponsePacketHeader(
-                            ResponseHeader(
-                                it.header.thrift.callId,
-                                StatusCode.OK,
-                                it.header.thrift.contentLength
-                            )
-                        ), Unpooled.wrappedBuffer(ByteArray(it.header.thrift.contentLength))
-                    )
+                    it.ok(buf)
                 }
             }
         }
